a299cf957f987a34f3808f3119ddd172e3140698
Aneeshteja <aneeshtejabattala@gmail.com>
2026-01-24 10:03:21 +0530

    Add files via upload

---
+++ AccessModiefiers.java
@@ -0,0 +1,15 @@
+class AccessModiefiers{
+
+private int id=255061;
+protected String name="aneesh";
+int roll_no=61;
+public String section="CSE A";
+
+public static void main(String[] args){
+AccessModiefiers obj=new AccessModiefiers();
+System.out.println(obj.name);
+System.out.println(obj.roll_no);
+System.out.println(obj.section);
+}
+
+}
